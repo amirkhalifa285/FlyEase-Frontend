adb4aaf0f8b8c636e861ad12dbb7207a201e7c69
amirkhalifa285 <amir.kh28@hotmail.com>
2025-02-01 04:24:45 +0200

    axios url update

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", // Backend base URL
+  baseURL: "https://flyease-backend-production.up.railway.app/api", // Backend base URL
   headers: {
     "Content-Type": "application/json",
   },
